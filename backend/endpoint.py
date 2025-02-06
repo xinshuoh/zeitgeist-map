456ea1db7cb8665753352a0964e402b96175d858
Robin Blake <rb2071@cam.ac.uk>
2025-02-06 13:00:23 +0000

    Setup database and created initial schema

--- backend/endpoint.py
+++ backend/endpoint.py
@@ -1,8 +1,14 @@
 # flask --app endpoint run
+import os
 
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
 
 # example
 @app.route("/top_arists")
