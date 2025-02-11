cff93f1267143928e806592c79db3ddb5abbd51e
Robin Blake <rb2071@cam.ac.uk>
2025-02-11 12:39:12 +0000

    Updated schema to have working songs, artists (and hopefully) genres

--- backend/dbupdate.py
+++ backend/dbupdate.py
@@ -1,4 +1,5 @@
 def setup(scheduler):
     @scheduler.task('interval', id='job_1', seconds=5)
     def job1():
-        print("Test")
+        # print("Test")
+        pass
--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
-from typing import List
+from __future__ import annotations
+from typing import List, Optional
 
-from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy import String, Integer, ForeignKey, Table, Column
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from flask_sqlalchemy import SQLAlchemy
@@ -8,24 +9,46 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Song(db.Model):
+    __tablename__ = "song"
+
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(128))
 
-    credits: Mapped[List['Credit']] = relationship(back_populates='song')
+    artists: Mapped[List[Artist]] = relationship(secondary='credit', back_populates="songs")
 
 
 class Artist(db.Model):
+    __tablename__ = "artist"
+
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(128))
 
-    credits: Mapped[List['Credit']] = relationship(back_populates='artist')
+    songs: Mapped[List[Song]] = relationship(secondary='credit', back_populates="artists")
+
+    genres: Mapped[List[Genre]] = relationship(secondary='has_genre', back_populates="artists")
 
 
-class Credit(db.Model):
+credit = Table(
+    "credit",
+    db.Model.metadata,
+    Column("song_id", ForeignKey("song.id")),
+    Column("artist_id", ForeignKey("artist.id"))
+)
+
+
+class Genre(db.Model):
+    __tablename__ = "genre"
+
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     
-    song_id: Mapped[int] = mapped_column(Integer, ForeignKey('song.id'))
-    song: Mapped[Song] = relationship(back_populates='credits')
+    name: Mapped[str] = mapped_column(String(128))
 
-    artist_id: Mapped[int] = mapped_column(Integer, ForeignKey('artist.id'))
-    artist: Mapped[Artist] = relationship(back_populates='credits')
+    artists: Mapped[List[Artist]] = relationship(secondary='has_genre', back_populates="genres")
+
+
+has_genre = Table(
+    "has_genre",
+    db.Model.metadata,
+    Column("artist_id", ForeignKey("artist.id")),
+    Column("genre_id", ForeignKey("genre.id"))
+)
