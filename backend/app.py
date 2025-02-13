80f925b9ed979dec40ff121a8ed39fd13b74935f
Robert Dancer <robertdancer@ymail.com>
2025-02-13 12:05:05 +0000

    First REST api query

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 # flask run
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
 from flask_cors import CORS, cross_origin
 from flask_apscheduler import APScheduler
+
+from urllib.parse import quote, unquote
 
 from models import *
 
@@ -31,15 +33,35 @@ def delete_tables():
 def build_tables():
     db.create_all()
 
+@app.cli.command("list-songs")
+def list_songs():
+    scalars = db.session.execute(db.select(Song)).scalars()
+    for s in scalars:
+        print(s.name, "-", s.artists[0].name)
+
+@app.cli.command("force-update")
+def force_update():
+    print(scheduler.get_job('kworb').func())
 
 # --- REST API ---
 
 # example
 @app.route("/ping")
 @cross_origin()
-def test():
+def ping():
     return "Hello from backend!"
 
 @app.route("/top_artists")
 def get_top_artists():
     return jsonify({'UK': ["Coldplay", "Pink Floyd"]})
+
+@app.route("/track_popularity")
+def track_popularity():
+    if 'song_id' in request.args:
+        vals = db.session.execute(db.select(Song).where(Song.id == request.args['song_id'])).scalars()
+    if 'name' in request.args:
+        vals = db.session.execute(db.select(Song).where(Song.name == unquote(request.args['name']))).scalars()
+    res = []
+    for v in vals:
+        res.append({'artist': v.artists[0].name})
+    return res
--- backend/dbupdate.py
+++ backend/dbupdate.py
@@ -4,14 +4,54 @@ from bs4 import BeautifulSoup
 from flask_apscheduler import APScheduler
 from flask_sqlalchemy import SQLAlchemy
 
-from sqlalchemy import sql
+from sqlalchemy import insert
 
 from models import *
 
 def setup(scheduler: APScheduler, db: SQLAlchemy):
 
-    @scheduler.task('interval', id='kworb', seconds=5)
+    @scheduler.task('interval', id='kworb', days=1)
     def kworb():
+        print("Updating from kworb...")
         with scheduler.app.app_context():
-            #print(db.session.execute(sql.text("SELECT * FROM songs")))
-            pass
+            def fetch_country(country):
+                contents = urllib.request.urlopen(f"https://kworb.net/spotify/country/{country}_daily.html").read()
+
+                soup = BeautifulSoup(contents, features="html.parser")
+
+                get_num = lambda s : int(s.replace(',','')) if s else None
+
+                for row in soup.find_all('tr')[1:]: # first row is headings
+                    elems = row.find_all('td')
+
+                    pos = get_num(elems[0].text)
+                    pos_change = None if elems[1].text in ('NEW', 'RE') else 0 if elems[1].text == '=' else get_num(elems[1].text)
+                    links = elems[2].find_all('a')
+                    artist = links[0].text
+                    artist_link = links[0].get('href')[10:-5]
+                    track = links[1].text
+                    track_link = links[1].get('href')[9:-5]
+                    # links[2:] has co-artists if they exists, ignoring rn
+                    days = get_num(elems[3].text)
+                    peak = get_num(elems[4].text)
+                    peak_x = get_num(elems[5].text[2:-1])
+                    streams = get_num(elems[6].text)
+                    streams_change = get_num(elems[7].text)
+                    week = get_num(elems[8].text)
+                    week_change = get_num(elems[9].text)
+                    total = get_num(elems[10].text)
+
+                    a = Artist(name = artist, songs = [], genres = [])
+                    db.session.add(a)
+
+                    s = Song(name = track, artists = [a])
+                    db.session.add(s)
+
+                    db.session.commit()
+
+                print("Updated " + country)
+
+            countries = ['ae']
+            #countries = ['ae', 'ar', 'at', 'au', 'be', 'bg', 'bo', 'br', 'by', 'ca', 'ch', 'cl', 'co', 'cr', 'cy', 'cz', 'de', 'dk', 'do', 'ec', 'ee', 'eg', 'es', 'fi', 'fr', 'gb', 'gr', 'gt', 'hk', 'hn', 'hu', 'id', 'ie', 'il', 'in', 'is', 'it', 'jp', 'kr', 'kz', 'lt', 'lu', 'lv', 'ma', 'mt', 'mx', 'my', 'ng', 'ni', 'nl', 'no', 'nz', 'pa', 'pe', 'ph', 'pk', 'pl', 'pt', 'py', 'ro', 'ru', 'sa', 'se', 'sg', 'sk', 'sv', 'th', 'tr', 'tw', 'ua', 'us', 'uy', 've', 'vn', 'za']
+            for country in countries:
+                fetch_country(country)
