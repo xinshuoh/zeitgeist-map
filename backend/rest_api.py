124a9d8022714de5576a227176900df43fdbdd34
Robin Blake <rb2071@cam.ac.uk>
2025-02-20 12:40:37 +0000

    Added country_compare and made spotify_id nullable

--- backend/rest_api.py
+++ backend/rest_api.py
@@ -18,7 +18,7 @@ def ping():
     return "Hello from backend!"
 
 @app.route("/track_popularity")
-@args(p("song_id")|p("name"))
+# @args(p("song_id")|p("name"))
 def track_popularity():
     if 'song_id' in request.args:
         vals = db.session.execute(db.select(Song).where(Song.id == request.args['song_id'])).scalars()
@@ -31,11 +31,15 @@ def track_popularity():
     return res
 
 @app.route("/country_top_tracks")
-@args(p("country_code"))
+# @args(p("country_code"))
 def country_top_tracks():
     current_date = dt.datetime.now().date()
     c = db.session.execute(db.select(Country).where(Country.code == request.args['country_code'])).scalar()
-    vals = db.session.execute(db.select(SongHasPopularity).where(SongHasPopularity.country == c, SongHasPopularity.date == current_date).order_by(SongHasPopularity.position)).scalars()
+    return get_top_tracks(c, current_date)
+
+
+def get_top_tracks(country, date):
+    vals = db.session.execute(db.select(SongHasPopularity).where(SongHasPopularity.country == country, SongHasPopularity.date == date).order_by(SongHasPopularity.position)).scalars()
     res = []
     for v in vals:
         res.append({
@@ -46,7 +50,7 @@ def country_top_tracks():
     return res
 
 @app.route("/song_country_history")
-@args(p("country_code")&(p("song_id")|p("song_name")))
+# @args(p("country_code")&(p("song_id")|p("song_name")))
 def song_country_history():
     # this might be really clunky
 
@@ -71,3 +75,47 @@ def song_country_history():
             'popularity': p.position
         })
     return res
+
+def get_today_track_names(country):
+    current_date = dt.datetime.now().date()
+    country_top_tracks = get_top_tracks(country, current_date)
+    tracks = set()
+    for entry in country_top_tracks:
+        tracks.add(entry['song_name'])
+    return tracks
+
+
+def get_percentage_similarity(c1):
+    
+    c1_tracks = get_today_track_names(c1)
+    countries = db.session.execute(db.select(Country)).scalars()
+    
+    max_similarity = 0
+    country_match = None
+    for country in countries:
+        if country.code != c1.code:
+            tracks = get_today_track_names(country)
+            similarity = len(set.intersection(tracks, c1_tracks)) / len(set.union(tracks, c1_tracks))
+            if similarity > max_similarity:
+                max_similarity = similarity
+                country_match = country
+    
+    return (country_match, max_similarity)
+
+
+@app.route('/country_compare')
+def country_compare():
+    c = db.session.execute(db.select(Country).where(Country.code == request.args['country_code'])).scalar()
+    # TODO : deal with when there is no match better
+    country, similarity = get_percentage_similarity(c)
+    if not country:
+        return None
+    else:
+        return {
+            'name': country.name,
+            'country_code': country.code,
+            'similarity': similarity
+        }
+        
+
+    
