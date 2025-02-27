bdc30df5c98f90584f82e92b1f06f7e4986a5b26
vl348 <vl348@cam.ac.uk>
2025-02-27 16:38:38 +0000

    up-to-date historical scraper

--- backend/scrape.py
+++ backend/scrape.py
@@ -9,6 +9,9 @@ from models import *
 from app import app
 from datetime import datetime
 
+
+#####
+#spotify-api, no longer in use
 client_id = 'b0d6aef0a4f846d3afe4dc5ab695bc3b'
 client_secret = 'ed600a63a1be4d2f83fc69f2be3169fe'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
@@ -27,6 +30,7 @@ def fetch_track_id(name):
     if len(items) > 0:
         track = items[0]
         return track['id']
+#####
     
 @app.cli.command("fetch-historical")
 def fetch_historical():
@@ -34,22 +38,20 @@ def fetch_historical():
 
     soup = BeautifulSoup(contents, features="html.parser")
 
-    for row in soup.find_all('a')[1:2]: # first row is headings
+    for row in soup.find_all('a')[1:11]: # first row is headings
 
         date_ = row.text
         contents_1 = urllib.request.urlopen(f"https://kworb.net/apple_songs/archive/{date_}").read()
         soup_1 = BeautifulSoup(contents_1, features="html.parser")
 
-        j = 0
         
         rows = soup_1.find_all('tr')
         popularity = {}
         countries = ["us","uk","jp","de","au","ca","fr","it","kr","mx","ru","th","be","br","ch","cn","co","es","hk","id","ie","in","nl","nz","tr","tw","za","ae","ar","at","cl","cz","dk","ec","ee","eg","fi","gr","hu","il","ke","kz","lb","lt","lu","my","ng","no","pe","ph","pl","pt","ro","sa","se","si","sg","sk","ua","vn"]
-        for row in rows[1:11]: # first row is headings
+        for row in rows[1:]: # first row is headings
             elems = row.find_all('td')
 
-            j += 1
-
+        
             #global stats
             pos = elems[0].text
             pos_change = None if elems[1].text in ('NEW', 'RE') else 0 if elems[1].text == '=' else elems[1].text
@@ -64,65 +66,60 @@ def fetch_historical():
             pts = elems[6].text
             pts_plus = elems[7].text
             tpts = elems[8].text
-            track_link = fetch_track_id(track)
-            print(date_, j)
+            
             date_time = datetime.strptime(date_[2:8], '%y%m%d')
 
             
-            #country specific stats
-
             a_list = []
 
-            s = db.session.execute(db.select(Song).where(Song.spotify_id == track_link)).scalar()
+            for artist in artists:
+                a = db.session.execute(db.select(Artist).where(Artist.name == artist)).scalar()
+                if not a:
+                    a = Artist(name = artist, songs = [], genres = [])
+                    db.session.add(a)
+                a_list.append(a)
+
+
+
+            s = db.session.execute(db.select(Song).where(Song.name == track)).scalar()
             if not s:
-                s = Song(name = track, artists = a_list, spotify_id = track_link)
+                s = Song(name = track, artists = a_list)
                 db.session.add(s)
 
 
+            #country specific stats
             for i in range(9, 69):
                 country_pos = elems[i].text
                 if country_pos:
                     country_pos = int(country_pos)
                     country = countries[i-9]
                     for artist in artists:
-                        #spotify artist id
-                        artist_link = fetch_artist_id(artist)
                         
-                        if artist_link in popularity:
-                            popularity[artist_link][country] += (1 / (1+(country_pos/20)))
+                        if artist in popularity:
+                            popularity[artist][country] += (1 / (1+(country_pos/20)))
                         else:
-                            popularity[artist_link] = defaultdict(int)
-                            popularity[artist_link][country] += (1 / (1+(country_pos/20)))
+                            popularity[artist] = defaultdict(int)
+                            popularity[artist][country] += (1 / (1+(country_pos/20)))
 
                     c = db.session.execute(db.select(Country).where(Country.code == country)).scalar()
-                    s_pop = SongHasPopularity(song = s, country = c, position = pos, date = date_time)
+
+                    s_pop = SongHasPopularity(song = s, country = c, position = country_pos, date = date_time)
                     db.session.add(s_pop)
 
-            for artist in artists:
-                a = db.session.execute(db.select(Artist).where(Artist.spotify_id == artist_link)).scalar()
-                if not a:
-                    #print("Insert")
-                    a = Artist(name = artist, songs = [], genres = [], spotify_id = artist_link)
-                    db.session.add(a)
-                a_list.append(a)
+
 
 
                 
         #artist popularity by country
-        print(date)
         for artist_key in popularity.keys():
             for country in popularity[artist_key].keys():
 
                 c = db.session.execute(db.select(Country).where(Country.code == country)).scalar()
-                a = db.session.execute(db.select(Artist).where(Artist.spotify_id == artist_key)).scalar()
+                a = db.session.execute(db.select(Artist).where(Artist.name == artist_key)).scalar()
 
                 a_pop = ArtistHasPopularity(artist = a, country = c, position = popularity[artist_key][country], date = date_time) 
                 db.session.add(a_pop)
 
             
         db.session.commit()
-
-
-
-
-#fetch_historical()
+        
