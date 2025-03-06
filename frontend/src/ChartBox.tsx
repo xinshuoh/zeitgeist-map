330d72d9e54516b10af78d3d77789cfdcbbfbd8a
Robin Blake <rb2071@cam.ac.uk>
2025-03-06 13:00:52 +0000

    styling tweaks

--- frontend/src/ChartBox.tsx
+++ frontend/src/ChartBox.tsx
@@ -14,7 +14,7 @@ const ChartBox = ({isOpen, song}: ChartBoxProps) => {
     return <div>
       <div className="w-[300px] rounded-lg shadow-md flex items-center justify-center" style={{ backgroundColor: '#d6b8c3' }}><button className={`inset-0 w-full h-full`}>                    
         
-        <div className={`bg-transparent rounded-md h-full overflow-hidden ${isOpen ? "w-full p-6" : "w-0 p-0"}`}>
+        <div className={`bg-transparent rounded-lg h-full overflow-hidden ${isOpen ? "w-full p-6" : "w-0 p-0"}`} style={{backgroundColor: '#d6b8c3'}}>
           <div className="text-lg font-bold">{song.song_name}</div>
           <div className="text-sm" style={{ color: "#361836" }}>{song.artist}</div>
       
