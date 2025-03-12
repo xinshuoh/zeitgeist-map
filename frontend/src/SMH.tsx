8616543e6a04b5335d6650ebcc5484fa4ef298d6
Xinshuo Huang <80334482+xinshuoh@users.noreply.github.com>
2025-03-12 21:23:52 +0000

    feat: icons for country compare and smh

--- frontend/src/SMH.tsx
+++ frontend/src/SMH.tsx
@@ -4,45 +4,62 @@ import { Popup as PopupComponent } from 'reactjs-popup';
 
 import { SpotifyAuth, Scopes } from 'react-spotify-auth'
 //import 'react-spotify-auth/dist/index.css'
-import React from 'react'
+import { useState } from 'react'
 //import { SpotifyApiContext } from 'react-spotify-api'
 import Cookies from 'js-cookie'
 
 import './SMH.css';
 
 export const SpiritualMusicalHome = () => {
-    const [token, setToken] = React.useState(Cookies.get("spotifyAuthToken"))
-  return (
-    <div className='app'>
-        <PopupComponent className="smhbox" trigger={        
-            <button className="compare">
-                Spiritual Musical Home
-            </button>}>
-            {token ? (async () => {
-                const res = await fetch("https://api.spotify.com/v1/me/playlists", {
-                    headers: {
-                        'Authorization':  `Bearer ${token}`
-                    }
-                });
-                const data = await res.json();
+    const [token, setToken] = useState(Cookies.get("spotifyAuthToken"))
+    const [isHovered, setIsHovered] = useState(false);
 
-                const playlists = data.items.filter((item:any) => item.public);
-                return playlists.map((item:any) => <button className="songbutton" onClick={() => {alert(item.id)}}>{item.name}</button>);
+    return (
+        <div className="relative flex items-center">
+            <PopupComponent
+                className="smhbox"
+                trigger={
+                    <button
+                        onMouseOver={() => setIsHovered(true)}
+                        onMouseOut={() => setIsHovered(false)}>
+                        <img src="smh_logo.svg" alt="Find your spiritual musical home"
+                            onMouseOver={(e) => e.currentTarget.src = 'smh_logo_hover.svg'}
+                            onMouseOut={(e) => e.currentTarget.src = 'smh_logo.svg'}
+                            className="cursor-pointer min-w-3 w-7 h-auto" />
+                    </button>}
+                closeOnDocumentClick
+            >
+                {token ? (async () => {
+                    const res = await fetch("https://api.spotify.com/v1/me/playlists", {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                    });
+                    const data = await res.json();
 
-            })() : 
-                <SpotifyAuth
-                    redirectUri='http://localhost:5173/callback'
-                    clientID='a33619059e2f4f34b1fb6b4439c75290'
-                    scopes={[Scopes.userReadPrivate, Scopes.playlistReadPrivate]} // either style will work
-                    onAccessToken={(token:any) => {
-                        console.log("Authenticated with Spotify");
-                    }}
-                    btnClassName='authbutton'
-                />
-            }
-            
+                    const playlists = data.items.filter((item: any) => item.public);
+                    return playlists.map((item: any) => <button className="songbutton" onClick={() => { alert(item.id) }}>{item.name}</button>);
+
+                })() :
+                    <SpotifyAuth
+                        redirectUri='http://localhost:5173/callback'
+                        clientID='a33619059e2f4f34b1fb6b4439c75290'
+                        scopes={[Scopes.userReadPrivate, Scopes.playlistReadPrivate]} // either style will work
+                        onAccessToken={(token: any) => {
+                            console.log("Authenticated with Spotify");
+                        }}
+                        btnClassName='authbutton'
+                    />
+                }
+
             </PopupComponent>
-        
-    </div>
-  )
+
+            {/* Tooltip - Only visible when hovered */}
+            {isHovered && (
+                <div className="absolute top-13 transform -translate-x-2/3 bg-gray-100 text-black text-sm px-2 py-1 rounded min-w-50 text-left rounded-lg shadow-lg">
+                    Find your spiritual musical home
+                </div>
+            )}
+        </div>
+    )
 }
